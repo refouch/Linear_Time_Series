aecf1338daa2763c5b3e4b90926931b8e48daf01
refouch <remi.foucherand@ensae.fr>
2025-04-08 18:09:43 +0200

    finished ARMA - BLOQUE

--- Time_Series.R
+++ Time_Series.R
@@ -10,7 +10,7 @@
 #Downloading packages (code borrowed online)
 list.of.packages <- c("readr", "zoo", "tseries", "stargazer", "fUnitRoots", "dplyr",
                       "aTSA", "xtable", "forecast", "ellipse", "graphics", "knitr", 
-                      "rmarkdown", "markdown")
+                      "rmarkdown", "markdown","rstudioapi")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
@@ -28,7 +28,7 @@ rm(list=ls())
 #######################################
 
 #Importing the series
-series <- read_csv2('valeurs_mensuelles.csv')
+series <- read_csv2('valeurs_mensuelles_jus.csv')
 
 #Cleaning unwanted data and renaming the columns
 series <- series[-c(1:3), ]
@@ -51,6 +51,7 @@ production <- zoo(series$Valeur, order.by = dates)
 #Dropping the last 4 values for prediction + creating differentiated series
 production_train <- production[1:(length(production)-4)]
 dproduction <- diff(production_train,1)
+ddproduction <- diff(dproduction,1)
 
 ########################
 ### PLOTTING BOTH SERIES
@@ -153,6 +154,94 @@ acf(y,24);pacf(y,24)
 dev.off()
 
 #Judging by the graph we then choose
-qmax <- 4
-pmax <- 1
+qmax <- 1
+pmax <- 5
 
+arima <- arima(y,c(1,0,3))
+Box.test(arima$residuals, lag=6, type="Ljung-Box", fitdf=5) #
+
+Qtests(arima$residuals, 24, 5)
+
+p_value <- #This function returns the p-values of the estimated ARMA
+  function(estim) {
+    coef <- estim$coef 
+    se <- sqrt(diag(estim$var.coef)) 
+    t <- coef / se 
+    pval <- (1 - pnorm(abs(t)))*2 
+    return(rbind(coef, se, pval))
+  }
+
+model_choice <- #This function estimates an arima and checks the fit and validity of the model with p-value
+  function(p, q, data = dproduction, k = 24) {
+    estim <-
+      try(arima(data, c(p, 0, q), optim.control = list(maxit = 20000)))
+    if (class(estim) == "try-error")
+      return(c(
+        "p" = p,
+        "q" = q,
+        "arsignif" = NA,
+        "masignif" = NA,
+        "resnocorr" = NA,
+        "ok" = NA
+      ))
+    arsignif <- if (p == 0) 
+      NA
+    else
+      p_value(estim)[3, p] <= 0.05 
+    masignif <- if (q == 0) 
+      NA
+    else
+      p_value(estim)[3, p + q] <= 0.05 
+    resnocorr <-
+      sum(Qtests(estim$residuals, 24, length(estim$coef) - 1)[, 2] <= 0.05, na.rm =
+            T) == 0 
+    checks <- c(arsignif, masignif, resnocorr) 
+    ok <-
+      as.numeric(sum(checks, na.rm = T) == (3 - sum(is.na(checks)))) 
+    return(
+      c(
+        "p" = p,
+        "q" = q,
+        "arsignif" = arsignif,
+        "masignif" = masignif,
+        "resnocorr" = resnocorr,
+        "ok" = ok
+      )
+    )
+  }
+
+arma_model_choice <- #This function runs the previous one with all p<pmax & q<qmax
+  function(pmax, qmax,data = dproduction) {
+    pqs <- expand.grid(0:pmax, 0:qmax) 
+    t(apply(matrix(1:dim(pqs)[1]), 1, function(row) { 
+      p <- pqs[row, 1]
+      q <- pqs[row, 2]
+      cat(paste0("Computing ARMA(", p, ",", q, ") \n"))
+      model_choice(p, q, data) 
+    }))
+  }
+
+armamodels <- arma_model_choice(pmax,qmax,dproduction) #estime tous les arima (patienter...)
+
+print(armamodels)
+selec <- armamodels[armamodels[,"ok"]==1&!is.na(armamodels[,"ok"]),] #modeles bien ajustes et valides
+selec
+### On a ? modeles bien ajustes et valides
+
+### PAS DE MODELES BIEN AJUSTÉS ET VALIDES A LA FOIS -> PROBLEMATIQUE
+## Peut-être que la Série n'est en fin de compte pas stationnaire ? On essaie avec la série différenciée deux fois juste pour être sûr...
+
+par(mfrow=c(1,2))
+acf(ddproduction,24);pacf(ddproduction,24)
+dev.off()
+
+plot(ddproduction)
+
+pmax <- 7
+qmax <- 2
+
+armamodels <- arma_model_choice(pmax,qmax, ddproduction) #estime tous les arima (patienter...)
+
+print(armamodels)
+selec <- armamodels[armamodels[,"ok"]==1&!is.na(armamodels[,"ok"]),] #modeles bien ajustes et valides
+selec
